aec3065d4bab33695d3330cbc6e04f694bfbb3d2
Lucas Minissale <lucas@sonru.com>
2018-06-25 20:08:34 +0100

    Installing postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_25_015842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "budgets", force: :cascade do |t|
     t.float "value"
     t.float "taxable_percentage"
