42c6d753a065c3cc127d1c8c836e606349497f31
Lucas Minissale <lucas@sonru.com>
2018-07-01 13:01:17 +0100

    Remove dependent destroy

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,15 +1,8 @@
 class Budget < ApplicationRecord
-  has_many :payments, dependent: :destroy
-  # after_create :create_payments
+  has_many :payments
 
-  validates_numericality_of :taxable_percentage, :greater_than_or_equal_to => 0.0, :less_than_or_equal_to => 100.0
+  validates_numericality_of :taxable_percentage, greater_than_or_equal_to: 0.0, less_than_or_equal_to: 100.0
 
-  private
-
-  # def create_payments
-  #   users = User.all
-  #   users.each { |u| u.generate_payment(value, due_date, id) }
-  # end
 end
 
 
