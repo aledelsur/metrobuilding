ce28f6969bdf9ffb9ac6cbc733b0b49cbd5f9824
Lucas Minissale <lucas@sonru.com>
2018-07-10 13:55:34 +0100

    Authentication issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "ayres-de-canning.herokuapp.com" }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host:'ayres-de-canning.herokuapp.com', port: '80' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'ayres-de-canning.herokuapp.com',
     user_name:            'ayresdecanning@gmail.com',
     password:             'emipilicata',
     authentication:       :plain,
