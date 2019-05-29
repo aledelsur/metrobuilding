cc406df21252a42db2967178f02cf0eb14af9e8a
Lucas Minissale <lucas@sonru.com>
2019-05-28 22:55:46 -0300

    Generating excel

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -3,19 +3,6 @@ class Receipt < ApplicationRecord
 
   validates_presence_of :concept, if: :nil_payment?
 
-  def self.to_csv
-    titles = %w{Nombre Monto Fecha Mes}
-    attributes = %w{name value formatted_created_at_date receipt_month}
-
-    CSV.generate(headers: true) do |csv|
-      csv << titles
-
-      all.each do |receipt|
-        csv << attributes.map{ |attr| receipt.send(attr) }
-      end
-    end
-  end
-
   def formatted_created_at_date
     created_at.strftime("%d/%m/%Y")
   end
