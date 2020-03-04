173d8886acd158a2b90d9aee09a36fd6558d38fa
Lucas Minissale <lucas@sonru.com>
2020-03-04 13:30:06 -0300

    Fixed issue with special debts

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -1,3 +1,18 @@
+function addRemoveIds(element){
+  var ids = $('#debt_property_ids').val().split(',');
+  ids.filter(String);
+  var newId = $(element).data('property-id').toString();
+  if(element.checked){
+    if(!ids.includes(newId)){
+      ids.push(newId);
+    }
+  } else {
+    ids[ids.indexOf(newId)] = '';
+    ids = ids.filter(String);
+  }
+  $('#debt_property_ids').val(ids.join(','));
+}
+
 $(document).ready(function(){
   $(document).on('click', 'button, a', function(){
     $(this).attr('disabled', true);
@@ -14,21 +29,15 @@ $(document).ready(function(){
   $(document).on('change', '.js-property-check', function() {
     // We need to conver user_ids to an array first
     // so we can know if an id is already there
-    var ids = $('#debt_property_ids').val().split(',');
-    ids.filter(String);
-    var newId = $(this).data('property-id').toString();
-    if(this.checked){
-      if(!ids.includes(newId)){
-        ids.push(newId);
-      }
-    } else {
-      ids[ids.indexOf(newId)] = '';
-      ids = ids.filter(String);
-    }
-    $('#debt_property_ids').val(ids.join(','));
+    addRemoveIds(this);
   })
 
   $(document).on('change', '#js-select-all', function(){
-    $('input[type="checkbox"]').prop('checked', $(this).prop('checked'))
+    $('input[type="checkbox"]').prop('checked', $(this).prop('checked'));
+    $('input[type="checkbox"]').each(function(index){
+      if(index != 0) { // Select all checkbox is index 0
+        addRemoveIds($('input[type="checkbox"]')[index]);
+      }
+    })
   })
 });
