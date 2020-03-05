d6406f1c7867035ec00c342cec14ff8a303ad355
Leandro <lealabra87@gmail.com>
2020-03-05 15:56:05 -0300

    Added Validations to Newsletters

--- app/controllers/admin/newsletters_controller.rb
+++ app/controllers/admin/newsletters_controller.rb
@@ -24,7 +24,7 @@ class Admin::NewslettersController < AdminController
   def create
     @newsletter = Newsletter.new(newsletter_params)
 
-    
+
     if @newsletter.save
       # @newsletter.images.attach(params[:newsletter][:images])
       if params[:save].present?
@@ -50,7 +50,6 @@ class Admin::NewslettersController < AdminController
         redirect_to edit_admin_newsletter_path(@newsletter, new_section: true) # redirect to edit view and build blank section
       end
     else
-      @newsletter = Newsletter.new
       render :edit
     end
   end
