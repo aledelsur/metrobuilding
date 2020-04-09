f540a30dc93d3fb82294e54ee71992e2998a1f2e
Lucas Minissale <lucas@sonru.com>
2020-04-09 18:00:05 -0300

    Fix bug

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -33,6 +33,18 @@ $(document).ready(function(){
     $('input[type="checkbox"]').prop('checked', $(this).prop('checked'))
   })
 
+  $('.datatable').DataTable({
+    // ajax: ...,
+    // autoWidth: false,
+    // pagingType: 'full_numbers',
+    // processing: true,
+    // serverSide: true,
+
+    // Optional, if you want full pagination controls.
+    // Check dataTables documentation to learn more about available options.
+    // http://datatables.net/reference/option/pagingType
+  });
+
   // $(document).on('click', '.newsletter-section .panel-heading', function(){
   //   var section = $(this).parent();
   //   section.find('.panel-body').toggle('collapse');
--- app/assets/javascripts/newsletter.js
+++ app/assets/javascripts/newsletter.js
@@ -5,3 +5,16 @@
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require jquery-ui/widgets/sortable
+
+
+$('.datatable').DataTable({
+  // ajax: ...,
+  // autoWidth: false,
+  // pagingType: 'full_numbers',
+  // processing: true,
+  // serverSide: true,
+
+  // Optional, if you want full pagination controls.
+  // Check dataTables documentation to learn more about available options.
+  // http://datatables.net/reference/option/pagingType
+});
