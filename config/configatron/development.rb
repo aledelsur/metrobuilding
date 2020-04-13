ae5cb905066d4a3ab16df8486a4ed9733199dd98
Lucas Minissale <lucas@sonru.com>
2020-04-13 17:42:52 -0300

    Added feature switch

--- config/configatron/development.rb
+++ config/configatron/development.rb
@@ -10,4 +10,10 @@ configatron.admin_password = '1234'
 configatron.s3_bucket = ENV['S3_BUCKET']
 configatron.s3_region = ENV['S3_REGION']
 configatron.s3_access_key_id = ENV['S3_ACCESS_KEY_ID']
-configatron.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+configatron.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']\
+
+
+configatron.logo = 'logo-metro.png'
+configatron.features.newsletters = true
+configatron.features.payments = false
+configatron.features.client_portal = false
