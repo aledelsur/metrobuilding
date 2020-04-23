ebc4ca6a90878e274608d1d5cf36552e308ba109
Alejandro Alvarez <alealvarez_00@hotmail.com>
2020-04-23 18:05:23 -0300

    Boton Enviar circular siempre presente

--- app/helpers/admin/newsletters_helper.rb
+++ app/helpers/admin/newsletters_helper.rb
@@ -1,2 +1,9 @@
 module Admin::NewslettersHelper
+  def index_newsletter_sent(newsletter)
+    if newsletter.sent_at.blank?
+      '-'
+    else
+      newsletter.sent_at&.strftime("%d-%m-%Y a las %H:%M hs.")
+    end
+  end
 end
