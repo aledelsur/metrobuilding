3191d2293fc140f48e1ba816c37300b64e42fb8a
Alejandro Alvarez <alealvarez_00@hotmail.com>
2020-04-24 15:44:21 -0300

    Hide and show Newsletters History feature on configatron

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -8,9 +8,9 @@
 
 configatron.app_url = ENV['APP_URL']
 configatron.logo = ENV['LOGO']
-configatron.features.newsletters = (ENV['NEWSLETTERS'] == 'true' || !ENV['NEWSLETTERS'].present?)
-configatron.features.newsletters_history = (ENV['NEWSLETTERS_HISTORY'] == 'true' || !ENV['NEWSLETTERS_HISTORY'].present?)
-configatron.features.payments = (ENV['PAYMENTS'] == 'true' || !ENV['PAYMENTS'].present?)
-configatron.features.client_portal = (ENV['PORTAL'] == 'true' || !ENV['PORTAL'].present?)
+configatron.features.newsletters = (ENV['NEWSLETTERS'] == 'true')
+configatron.features.newsletters_history = (ENV['NEWSLETTERS_HISTORY'] == 'true')
+configatron.features.payments = (ENV['PAYMENTS'] == 'true')
+configatron.features.client_portal = (ENV['PORTAL'] == 'true')
 
 configatron.intranet_name = 'Metro 19' # i.e: "metro_19"
