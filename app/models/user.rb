5fde8f66256bc1e8a089fca682233c7880eda270
Lucas Minissale <lucas@sonru.com>
2020-05-04 12:23:24 -0300

    Reverse

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,6 @@ class User < ApplicationRecord
   end
 
   def debt(budgets = nil)
-    budgets = Budget.active.all if budgets.nil?
     total_to_pay = 0
     budgets.each do |budget|
       total_percentage = properties.sum { |property| property.percentage }
