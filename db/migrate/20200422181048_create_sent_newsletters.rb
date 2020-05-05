bbd5b93335375320ff4847f6b342287c66e16468
Lucas Minissale <lucas@sonru.com>
2020-05-04 23:59:11 -0300

    R

--- db/migrate/20200422181048_create_sent_newsletters.rb
+++ db/migrate/20200422181048_create_sent_newsletters.rb
@@ -1,11 +1,11 @@
 class CreateSentNewsletters < ActiveRecord::Migration[5.2]
   def change
-    create_table :sent_newsletters do |t|
-      t.string :sent_newsletter_token, index: true
-      t.belongs_to :user
-      t.belongs_to :newsletter
-
-      t.timestamps
-    end
+    # create_table :sent_newsletters do |t|
+    #   t.string :sent_newsletter_token, index: true
+    #   t.belongs_to :user
+    #   t.belongs_to :newsletter
+    #
+    #   t.timestamps
+    # end
   end
 end
--- db/migrate/20200423205534_add_newsletter_variables.rb
+++ db/migrate/20200423205534_add_newsletter_variables.rb
@@ -1,5 +1,5 @@
 class AddNewsletterVariables < ActiveRecord::Migration[5.2]
   def change
-    add_column :sent_newsletters, :newsletter_variables, :json, default: {}
+    # add_column :sent_newsletters, :newsletter_variables, :json, default: {}
   end
 end
