46fe7a1002d9c5c7ebd01aeb4833e8c81d2587c0
Lucas Minissale <lucas@sonru.com>
2020-05-05 00:57:20 -0300

    Fix mess

--- db/migrate/20200505034354_fix_mess.rb
+++
@@ -1,8 +0,0 @@
-class FixMess < ActiveRecord::Migration[5.2]
-  def change
-    add_column :budgets, :start_date, :datetime
-    # add_column :newsletters, :email_content, :text
-    add_column :budgets, :rate_1, :float, default: 1
-    add_column :budgets, :rate_2, :float, default: 1
-  end
-end
