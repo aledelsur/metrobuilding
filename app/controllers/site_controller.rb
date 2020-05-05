bcf300e1f2989aa59ec141a9097580b6591ca423
Lucas Minissale <lucas@sonru.com>
2020-05-05 01:23:54 -0300

    Configatron feature client portal

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,6 @@ class SiteController < ApplicationController
   before_action :feature_enabled?, :authenticate_user!
 
   def feature_enabled?
-    redirect_to admin_users_path unless configatron.features.client_portal
+    redirect_to admin_users_path unless @company.has_feature?(:client_portal)
   end
 end
