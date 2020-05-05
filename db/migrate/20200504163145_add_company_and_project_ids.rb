ba3fe8a173fadcd896efb478051e80b1f3fd0a4e
Lucas Minissale <lucas@sonru.com>
2020-05-04 23:02:53 -0300

    Moved migration

--- db/migrate/20200504163145_add_company_and_project_ids.rb
+++ db/migrate/20200504163145_add_company_and_project_ids.rb
@@ -31,28 +31,5 @@ class AddCompanyAndProjectIds < ActiveRecord::Migration[5.2]
 
     add_column :admin_users, :project_id, :bigint
     add_column :admin_users, :company_id, :bigint
-
-    populate_existing_company
-  end
-
-  def populate_existing_company
-    company = Company.create(name: 'Scotto')
-    project = Project.create(name: 'ayres-de-canning', company_id: company.id)
-
-    User.update_all(company_id: company.id)
-    Budget.update_all(company_id: company.id, project_id: project.id)
-    Debt.update_all(company_id: company.id, project_id: project.id)
-    Property.update_all(company_id: company.id, project_id: project.id)
-    Payment.update_all(company_id: company.id, project_id: project.id)
-    PropertyCategory.update_all(company_id: company.id, project_id: project.id)
-    MediaAsset.update_all(company_id: company.id, project_id: project.id)
-    Receipt.update_all(company_id: company.id, project_id: project.id)
-    Newsletter.update_all(company_id: company.id, project_id: project.id)
-    SentNewsletter.update_all(company_id: company.id, project_id: project.id)
-    AdminUser.update_all(company_id: company.id, project_id: project.id)
-
-    User.all.each do |user|
-      UserProject.create(user_id: user.id, project_id: project.id)
-    end
   end
 end
--- db/migrate/20200504203852_create_company_features.rb
+++ db/migrate/20200504203852_create_company_features.rb
@@ -12,14 +12,39 @@ class CreateCompanyFeatures < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
+    populate_existing_company
+
     Company.all.each do |company|
       FEATURES.each do |feature_key|
+        next if company.company_features.find_by(feature: feature_key)
+
         company.company_features.create(
           feature: feature_key,
           boolean_value: (Company.feature_value_type(feature_key) == 'Boolean' ? Company.feature_default_value(feature_key) : nil),
           string_value: (Company.feature_value_type(feature_key) == 'String' ? Company.feature_default_value(feature_key) : nil)
         )
       end
+    end
+  end
+
+  def populate_existing_company
+    company = Company.create(name: 'Scotto')
+    project = Project.create(name: 'ayres-de-canning', company_id: company.id)
+
+    User.update_all(company_id: company.id)
+    Budget.update_all(company_id: company.id, project_id: project.id)
+    Debt.update_all(company_id: company.id, project_id: project.id)
+    Property.update_all(company_id: company.id, project_id: project.id)
+    Payment.update_all(company_id: company.id, project_id: project.id)
+    PropertyCategory.update_all(company_id: company.id, project_id: project.id)
+    MediaAsset.update_all(company_id: company.id, project_id: project.id)
+    Receipt.update_all(company_id: company.id, project_id: project.id)
+    Newsletter.update_all(company_id: company.id, project_id: project.id)
+    SentNewsletter.update_all(company_id: company.id, project_id: project.id)
+    AdminUser.update_all(company_id: company.id, project_id: project.id)
+
+    User.all.each do |user|
+      UserProject.create(user_id: user.id, project_id: project.id)
     end
   end
 end
