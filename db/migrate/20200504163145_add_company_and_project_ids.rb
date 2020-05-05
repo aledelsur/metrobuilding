b0f8e3844505b0298f975d9a4b9d0bb4921cf229
Lucas Minissale <lucas@sonru.com>
2020-05-05 00:03:56 -0300

    Fixing migrations

--- db/migrate/20200504163145_add_company_and_project_ids.rb
+++ db/migrate/20200504163145_add_company_and_project_ids.rb
@@ -1,21 +1,21 @@
 class AddCompanyAndProjectIds < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :company_id, :bigint
-
-    add_column :budgets, :project_id, :bigint
-    add_column :budgets, :company_id, :bigint
-
-    add_column :debts, :project_id, :bigint
-    add_column :debts, :company_id, :bigint
-
-    add_column :receipts, :project_id, :bigint
-    add_column :receipts, :company_id, :bigint
-
-    add_column :media_assets, :project_id, :bigint
-    add_column :media_assets, :company_id, :bigint
-
-    add_column :newsletters, :project_id, :bigint
-    add_column :newsletters, :company_id, :bigint
+    # add_column :users, :company_id, :bigint
+    #
+    # add_column :budgets, :project_id, :bigint
+    # add_column :budgets, :company_id, :bigint
+    #
+    # add_column :debts, :project_id, :bigint
+    # add_column :debts, :company_id, :bigint
+    #
+    # add_column :receipts, :project_id, :bigint
+    # add_column :receipts, :company_id, :bigint
+    #
+    # add_column :media_assets, :project_id, :bigint
+    # add_column :media_assets, :company_id, :bigint
+    #
+    # add_column :newsletters, :project_id, :bigint
+    # add_column :newsletters, :company_id, :bigint
 
     add_column :payments, :project_id, :bigint
     add_column :payments, :company_id, :bigint
