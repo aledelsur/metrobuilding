425af99ef6a4032cb800033285fac855144691b8
Alejandro Alvarez <alealvarez_00@hotmail.com>
2020-05-19 20:17:44 -0300

    Action Mailer default_url_options depending on project name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,4 +5,8 @@ class ApplicationMailer < ActionMailer::Base
   def apply_smtp_settings_for_project(project)
     ActionMailer::Base.smtp_settings.merge!(project.smtp_settings)
   end
+
+  def apply_url_host(project)
+    ActionMailer::Base.default_url_options = { host: "#{project.name}.buildit.com.ar" }
+  end
 end
--- app/mailers/investor_mailer.rb
+++ app/mailers/investor_mailer.rb
@@ -1,13 +1,14 @@
 class InvestorMailer < ApplicationMailer
   def send_newsletter(sent_newsletter)
     apply_smtp_settings_for_project(sent_newsletter.project)
+    apply_url_host(sent_newsletter.project)
 
     @sent_newsletter = sent_newsletter
     @user = sent_newsletter.user
 
     mail(
       to: @user.email,
-      subject: 'Tu nueva Newsletter de Metrobuilding'
+      subject: "Tu nueva Circular de #{sent_newsletter.project.name.humanize}"
     )
   end
 end
