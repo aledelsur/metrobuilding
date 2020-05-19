51a2d6aaea30abff7a47a4d1ddfa5deb2e460f1d
Alejandro Alvarez <alealvarez_00@hotmail.com>
2020-05-19 19:25:53 -0300

    Added authentication to Sidekiq in production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :admin_users
   require 'sidekiq/web'
-  mount Sidekiq::Web => "/sidekiq"
+
+  scope :monitoring do
+    # Sidekiq Basic Auth from routes on production environment
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_USERNAME"])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_PASSWORD"]))
+    end if Rails.env.production?
+
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
 
   namespace :admin do
     resources :receipts
