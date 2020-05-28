2ba78de7aef8f044a3648335f41c5cb6a13b409b
Leandro <lealabra87@gmail.com>
2020-05-28 18:58:59 -0300

    restrict uploadable file types, must be only jpg and png in media asset

--- app/models/media_asset.rb
+++ app/models/media_asset.rb
@@ -15,6 +15,8 @@ class MediaAsset < ApplicationRecord
   belongs_to :newsletter_section, optional: true
   has_and_belongs_to_many :newsletter_sections
   has_one_attached :image
+  has_one_attached :document
+
 
   belongs_to :company
   belongs_to :project
@@ -22,6 +24,7 @@ class MediaAsset < ApplicationRecord
   validates :project_id, :company_id, presence: true
 
   validate :image_attached?
+  validate :correct_document_mime_type
 
   def thumbnail
     return image.variant(resize: '120x120!') if image.attached?
@@ -31,5 +34,11 @@ class MediaAsset < ApplicationRecord
 
   def image_attached?
     errors.add(:image, "debe ser seleccionada") unless image.attached?
+  end
+
+  def correct_document_mime_type
+    if document.attached? && !document.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:document, 'Las imagenes deben ser JPG o PNG')
+    end
   end
 end
